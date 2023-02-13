044f8cfb58b5169017fe5df8239a4dc52a98e243
web-diy <dizain.ilya@yandex.ru>
2023-02-13 16:57:46 +0300

    13/02/23

--- index.js
+++ index.js
@@ -5,12 +5,32 @@ const token = '6183373370:AAG1HeGZGA0zyuV3kdmqAfSJF124kKDq8Xo';
 
 const bot = new TelegramApi(token, {polling:true})
 
-bot.on('message', msg => {
-    const text = msg.text;
-    const chatId = msg.chat.id;
-    console.log(msg)
-}) 
+ 
 
+const start = () => {
+    bot.setMyCommands([
+        {command:'/start', description: 'Начальное приветствие!'},
+        {command: '/info', description: 'Получить информацию о пользователе'}
+    ]
+    )
+    
+    bot.on('message', async msg => {
+        const text = msg.text;
+        const chatId = msg.chat.id;
+        if (text == '/start'){
+            await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/2af/6db/2af6dbf1-4758-4b9a-8bd5-8f9d49af90f6/11.webp')
+            return bot.sendMessage(chatId, 'Добро пожаловать в тестовый телеграмм-бот');
+        }
+        if (text == '/info'){
+            return bot.sendMessage(chatId, `Ваше имя ${msg.from.first_name}`);
+        }
+
+        return bot.sendMessage(chatId, 'Не распознанная команда!');
+        console.log(msg)
+    })
+}
+
+start()
 
 
  
