11959a7baf559921e12396b3478eb17bd585a52c
web-diy <dizain.ilya@yandex.ru>
2023-02-13 16:15:18 +0300

    const

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const token = '6183373370:AAG1HeGZGA0zyuV3kdmqAfSJF124kKDq8Xo';
 const bot = new TelegramApi(token, {polling:true})
 
 bot.on('message', msg => {
+    const text = msg.text;
+    const chatId = msg.chat.id;
     console.log(msg)
 }) 
 
