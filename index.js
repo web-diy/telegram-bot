9b04b01f297ba8c30af2e96a205080d1a79e8c4a
web-diy <dizain.ilya@yandex.ru>
2023-02-21 15:18:50 +0300

    21/02

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 /* My First Telegram Bot */
 const TelegramApi = require('node-telegram-bot-api')
-
 const token = '6183373370:AAG1HeGZGA0zyuV3kdmqAfSJF124kKDq8Xo';
-
 const bot = new TelegramApi(token, {polling:true})
+const {gameOptions, againOptions} = require('./options.js');
+const chats = {} 
 
- 
-
+const startGame = async (chatId) => {
+        await bot.sendMessage(chatId, `Сейчас я загадаю цифру от 0 до 9, а Вы должны попробовать его отгадать`);
+        const randomNumber = Math.floor(Math.random()*10);
+        chats[chatId] = randomNumber;
+        await bot.sendMessage(chatId, 'Отгадывай', gameOptions);
+}
 const start = () => {
     bot.setMyCommands([
         {command:'/start', description: 'Начальное приветствие!'},
-        {command: '/info', description: 'Получить информацию о пользователе'}
+        {command: '/info', description: 'Получить информацию о пользователе'},
+        {command: '/game', description: 'Играть в игру'}
     ]
     )
     
@@ -24,13 +29,29 @@ const start = () => {
         if (text == '/info'){
             return bot.sendMessage(chatId, `Ваше имя ${msg.from.first_name}`);
         }
+        if (text == '/game'){
+            return startGame(chatId);
+        }
+        console.log(msg);
+        return bot.sendMessage(chatId, 'Нераспознанная команда!');
+    })
 
-        return bot.sendMessage(chatId, 'Не распознанная команда!');
-        console.log(msg)
+    bot.on('callback_query', async msg => {
+        const data = msg.data;
+        const chatId = msg.message.chat.id;
+        if (data == '/again'){
+            return startGame(chatId);
+        }
+
+        if (data == chats[chatId]){
+            return await bot.sendMessage(chatId, `Вы отгадали цифру ${chats[chatId]}! Поздравляем!`, againOptions);
+        }
+        else return await bot.sendMessage(chatId, `Вы не отгадали! Была загадана цифра ${chats[chatId]} Попробуйте ещё раз...`, againOptions)
+        //bot.sendMessage(chatId, `Вы выбрали цифру ${data}`);
     })
 }
 
-start()
+start();
 
 
  
